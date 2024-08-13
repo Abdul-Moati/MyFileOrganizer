1d6f7fa0823af8f52d76b3aad170d9df4b17b66f
Abdul-Moati <abdulmoati.diab@gmail.com>
2024-08-13 10:04:53 +0300

    Finalized the program.

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ Description: Develop a tool that helps users organize files on their computer.
 Users can select a directory, and the program will automatically categorize files
 based on their extensions (e.g., images, documents, videos) into folders.
 
-Challenge: Handling large numbers of files efficiently, dealing with potential file conflicts
-and providing options for undoing actions.
+Challenge: Handling large numbers of files efficiently and dealing with potential file conflicts.
 """
 
 # Libraries
 import FreeSimpleGUI as fsg
+import shutil
+import os
 
 # Aesthetics
 background_color = '#FFD580'
@@ -59,17 +60,17 @@ desired_extensions_input_box = fsg.InputText(size=(55, 3), tooltip="Extensions",
 
 
 # Check Boxes
-video_check_box = fsg.Checkbox(['Video (.mp4)'], default=False, key='.mp4', background_color='grey')
-image_check_box = fsg.Checkbox(['Image (.png/.jpg/.jpeg)'], default=False, key='.png .jpg. jpeg', background_color='grey')
-audio_check_box = fsg.Checkbox(['Audio (.mp3'], default=False, key='.mp3', background_color='grey')
+video_check_box = fsg.Checkbox(['Video (.mp4/.mov/.mkv)'], default=False, key='.mp4 .mov .mkv', background_color='grey')
+image_check_box = fsg.Checkbox(['Image (.png/.jpg/.jpeg)'], default=False, key='.png .jpg jpeg', background_color='grey')
+audio_check_box = fsg.Checkbox(['Audio (.mp3/.wav/.flac'], default=False, key='.mp3 .wav .flac', background_color='grey')
 
 pdf_check_box = fsg.Checkbox(['PDF (.pdf)'], default=False, key='.pdf', background_color='grey')
 text_check_box = fsg.Checkbox(['Text (.txt)'], default=False, key='.txt', background_color='grey')
-archive_check_box = fsg.Checkbox(['Archive (.zip/.rar)'], default=False, key='.zip .rar', background_color='grey')
+archive_check_box = fsg.Checkbox(['Archive (.zip/.rar/.7z)'], default=False, key='.zip .rar', background_color='grey')
 
-word_check_box = fsg.Checkbox(['Word (.doc/.docx)'], default=False, key='.doc', background_color='grey')
-powerpoint_check_box = fsg.Checkbox(['Powerpoint (.ppt/.pptx)'], default=False, key='.ppt', background_color='grey')
-Excel_check_box = fsg.Checkbox(['Excel (.xls/.xlsx)'], default=False, key='.xls', background_color='grey')
+word_check_box = fsg.Checkbox(['Word (.doc/.docx/.gdoc)'], default=False, key='.doc .docx .gdoc', background_color='grey')
+powerpoint_check_box = fsg.Checkbox(['Powerpoint (.ppt/.pptx/.gslides)'], default=False, key='.ppt .pptx .gslides', background_color='grey')
+Excel_check_box = fsg.Checkbox(['Excel (.xls/.xlsx/.gsheet)'], default=False, key='.xls .xlsx .gsheet', background_color='grey')
 
 
 
@@ -86,7 +87,7 @@ clmn = fsg.Column(
     ,justification='right', background_color=background_color)
 
 
-
+# GUI Layout
 layout = [
     [welcome_label],
     [explanation_label],
@@ -107,7 +108,7 @@ layout = [
 # Main Window
 window = fsg.Window('My Folder Organizer', layout, background_color='#FFD580')
 
-true_check_boxes = []
+checked_extensions = []
 
 while True:
     event, values = window.read()
@@ -117,14 +118,61 @@ while True:
     print(values["EXTENSIONS"])
 
     for key, value in values.items():
-        if value:
-            true_check_boxes.append(key)
 
-    print(true_check_boxes)
+        # Keep this, it is not redundant!
+        if value == True:
+            checked_extensions.extend(key.split())
+        if key == "EXTENSIONS":
+            checked_extensions.extend(value.split())
 
+    print(checked_extensions)
 
+    # Set the path to the desired folder
+    directory = values["DIRECTORY"]
 
+    for extension in checked_extensions:
+        destination_folder = os.path.join(directory, extension[1:].upper() + " Folder")
+        print(type(destination_folder))
+        print(f"destination folder: {destination_folder}")
 
+        # Creating the folder in case it does not exist
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        # Change the working directory to Downloads
+        os.chdir(directory)
+
+        # Iterate over the folders and files in Downloads
+        for folder, subfolders, files in os.walk(directory):
+            print(f"We Are HEEEEEREEEE: {folder}")
+
+            # Clear the subfolders list to prevent walking into subdirectories
+            subfolders.clear()
+
+            incfile = 1
+            for file in files:
+                print(f"File{incfile}: {file}")
+                if file.lower().endswith(extension):
+                    print(f"File has {extension}!!")
+                    source_path = os.path.join(folder, file)
+
+                    # Move the file to folder
+                    shutil.move(source_path, destination_folder)
+                    print(f"File Moved to {destination_folder}!!")
+                incfile += 1
+
+            print("\n")
+
+    # After moving all files, check and delete empty folders
+    for folder, subfolders, files in os.walk(directory, topdown=False):  # Traverse in bottom-up order
+        if not os.listdir(folder):  # Check if the directory is empty
+            os.rmdir(folder)  # Remove the empty directory
+            print(f"Deleted empty directory: {folder}")
+
+    # Closing the window when the Organize My Folder button is pressed
+    break
+
+    # Closing the window when the close button on the top right is pressed
     if event == fsg.WINDOW_CLOSED:
             break
 
